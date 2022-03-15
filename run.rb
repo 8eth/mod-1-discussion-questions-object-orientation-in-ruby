291c1c5dbe408098d66423a802a417a688ef8a36
Betelihem <Betelihem>
2022-03-15 09:18:29 -0700

    commit tree 9a4b12fec968fa6f238cd29d658d39db5ce7366a

---
+++ run.rb
@@ -0,0 +1,62 @@
+# class Species
+
+#     def animal
+#       "cat"
+#     end
+  
+# end 
+
+# rose = Species.new
+# puts rose.animal
+
+
+# class Animal
+
+#     def species
+#       "cat"
+#     end
+
+# end
+  
+# maru = Animal.new
+# hanna = Animal.new 
+
+
+# puts maru == hanna 
+
+# class Animal
+
+#     def species 
+#       @my_species = "cat"
+#       puts "Hi! I'm a #{@my_species}"
+#     end
+
+# end 
+
+# maru = Animal.new
+# maru.species 
+
+# class  Animal
+#     def initialize(species)
+#         @species = species
+#     end
+
+#     def species 
+#         @species
+#     end
+# end
+
+# frederick = Animal.new("bull")
+# puts frederick.species
+# # => "bull"
+
+
+# class Animal
+
+#     def initialize(species)
+#       @species = species
+#     end
+  
+# end
+  
+# lil_bub = Animal.new("cat") 
